ae233cc761c6fb9ebb04582a9f97c2cc0500b7c7
fsorek <filip.sorek96@gmail.com>
2020-01-12 01:27:58 +0100

    attack button, player attack base implementation

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public void Attack()
+    {
+        Debug.Log("ATTACC");
+        // collider cast forward and deal damage
+    }
+}
