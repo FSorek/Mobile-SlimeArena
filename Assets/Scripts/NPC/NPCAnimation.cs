28e876fd99ce21d2fafcf49c52276262f1118a33
fsorek <filip.sorek96@gmail.com>
2020-02-24 21:32:07 +0100

    enemy sprites now flip properly

--- Assets/Scripts/NPC/NPCAnimation.cs
+++ Assets/Scripts/NPC/NPCAnimation.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
-[RequireComponent(typeof(EnemyNPC))]
+﻿using System;
+using UnityEngine;
+[RequireComponent(typeof(IEnemyStateMachine))]
 public class NPCAnimation : EntityAnimator<IEnemyStateMachine>
 {
     private static readonly int IsDead = Animator.StringToHash("IsDead");
     private static readonly int IsMoving = Animator.StringToHash("IsMoving");
     private static readonly int IsRepositioning = Animator.StringToHash("IsRepositioning");
     private bool startsFlipped;
+    private float lastPositionX;
 
     private void Start()
     {
@@ -19,5 +21,16 @@ public class NPCAnimation : EntityAnimator<IEnemyStateMachine>
         animator.SetBool(IsRepositioning, state is NPCRepositionState);
         if(state is NPCDeadState)
             animator.SetTrigger(IsDead);
+    }
+
+    private void Update()
+    {
+        var currentPositionX = transform.position.x;
+        if(lastPositionX == currentPositionX)
+            return;
+        bool shouldFlip = startsFlipped ? lastPositionX - currentPositionX > 0 : lastPositionX - currentPositionX < 0;
+        if(renderer.flipX != shouldFlip)
+            renderer.flipX = shouldFlip;
+        lastPositionX = currentPositionX;
     }
 }
