034290f24af2939c87e8b6acd5e14f9ca3fc66a3
fsorek <filip.sorek96@gmail.com>
2020-03-08 22:34:08 +0100

    Created initial powerup manager class

---
+++ Assets/Scripts/Game/PowerupManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+    private Player player;
+    private bool canSpawnHealthUp;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        player.Health.OnTakeDamage += PlayerOnTakeDamage;
+    }
+
+    private void PlayerOnTakeDamage(int damage)
+    {
+        canSpawnHealthUp = true;
+    }
+}
